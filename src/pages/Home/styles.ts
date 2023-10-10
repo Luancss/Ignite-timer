b84f02b0e357dc1063a8a6ba9477f7bcfa62e7d2
Luancss <ventobo123@gmail.com>
2023-10-10 15:06:22 -0300

    Página - History

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -62,10 +62,12 @@ const BaseInput = styled.input`
   font-size: 1.125rem;
   padding: 0 0.5rem;
   color: ${(props) => props.theme['gray-100']};
+  
   &:focus {
     box-shadow: none;
     border-color: ${(props) => props.theme['green-500']};
   }
+
   &::placeholder {
     color: ${(props) => props.theme['gray-500']};
   }
